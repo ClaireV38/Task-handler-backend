8671fb79f4fa74c8041d8694ea134827939eab51
Claire Verdier <claire.verdier@orkester.fr>
2025-09-12 15:40:58 +0200

    update docker compose wuth user

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\CurrentUserController;
 
@@ -12,7 +13,7 @@ Route::get('/me', [CurrentUserController::class, 'show'])
     ->name('user.show');
 
 Route::get('/tasks', [TaskController::class, 'index'])
-    ->middleware(['auth:sanctum', 'role:admin']);
+    ->middleware(['auth:sanctum']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();

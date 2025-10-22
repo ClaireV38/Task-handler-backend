e4bb04d5a32d9c261437e4c360f82f0b53e93938
Claire Verdier <claire.verdier@orkester.fr>
2025-10-22 15:25:42 +0200

    add Tasks factory and TaskSeeder

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
     protected $fillable = [
         'title',
         'description',
